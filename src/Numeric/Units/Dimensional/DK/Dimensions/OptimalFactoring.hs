7ea2f63d6ae6126e24d73e6e5f0bc58660c7aecc
Douglas McClean <douglas.mcclean@gmail.com>
2015-09-22 20:48:28 -0400

    Closer, but still wrong.

--- src/Numeric/Units/Dimensional/DK/Dimensions/OptimalFactoring.hs
+++ src/Numeric/Units/Dimensional/DK/Dimensions/OptimalFactoring.hs
@@ -4,12 +4,15 @@ module Numeric.Units.Dimensional.DK.Dimensions.OptimalFactoring
 where
 
 import Data.Foldable
+import Data.List (sort)
 import Data.Ord
 import Data.Proxy
 import Numeric.Units.Dimensional.DK.Dimensions.TermLevel
 import Numeric.Units.Dimensional.DK.Quantities
-import Prelude hiding ((/), (*), recip)
+import Prelude hiding ((/), (*), recip, any, sum)
 import qualified Prelude as P
+
+import Debug.Trace
 
 type Cost = Double
 
@@ -29,19 +32,20 @@ siCostMap = id
           $ insert (dimension (Proxy :: Proxy DElectricPotential), 0.5)
           $ defaultCostMap
 
-minCost :: CostMap -> Dimension' -> [Dimension']
-minCost (CostMap gs) d = fst $ minCost' d gs' emptyPath
+minCost :: CostMap -> Dimension' -> Path
+minCost (CostMap gs) d = minCost' d gs''' emptyPath (naiveCost d)
   where
-    gs' = filter (relevant d) gs
+    gs''' = sort gs''
+    gs'' = gs' ++ fmap (\(s,c) -> (recip s,c)) gs'
+    gs' = filter (\(d',_) -> relevant d d') gs
 
-minCost' :: Dimension' -> [Step] -> Path -> Path
-minCost' d [] _ | d == dOne = emptyPath
-                | otherwise = ([d], 1 P./ 0) -- we really shouldn't reach here because we have all the generators as possible steps to start with
-minCost' d gs p = leastCostly [minCost' (d / ds) (filter (relevant (d / ds)) gs) (extend p s) | s@(ds, _) <- possibleSteps gs]
+minCost' :: Dimension' -> [Step] -> Path -> Cost -> Path
+minCost' d _  p@(_, c) lim | d == dOne = p
+                           | c >= lim  = ([d], 1 P./ 0)
+minCost' d [] _        _   = ([d], 1 P./ 0) -- we really shouldn't reach here because we have all the generators as possible steps to start with
+minCost' d gs p@(_, c) lim = leastCostly [minCost' (d / ds) (retainRelevant ds gs) (extend p s) lim | s@(ds, _) <- gs]
   where
-    possibleSteps :: [Step] -> [Step]
-    possibleSteps [] = []
-    possibleSteps ((s, cs) : steps) = (s, cs) : (recip s, cs) : possibleSteps steps
+    retainRelevant ds = filter (\(ds',_) -> relevant (d / ds) ds' && ds' /= recip ds && ds <= ds')
 
 emptyPath :: Path
 emptyPath = ([], 0)
@@ -49,14 +53,11 @@ emptyPath = ([], 0)
 extend :: Path -> Step -> Path
 extend (p, cp) (s, cs) = (s:p, cp + cs)
 
-relevant :: Dimension' -> Step -> Bool
-relevant d1 (d2, _) = relevantD d1 d2
-
-relevantD :: Dimension' -> Dimension' -> Bool
-relevantD d1 d2 = any id $ zipWith (&&) (fmap (/= 0) $ asList d1) (fmap (/= 0) $ asList d2)
+relevant :: Dimension' -> Dimension' -> Bool
+relevant d1 d2 = any id $ zipWith (&&) (fmap (/= 0) $ asList d1) (fmap (/= 0) $ asList d2)
 
 naiveCost :: Dimension' -> Cost
-naiveCost = fromIntegral . sum . asList
+naiveCost = fromIntegral . sum . fmap abs . asList
 
 leastCostly :: [(a, Cost)] -> (a, Cost)
 leastCostly = minimumBy (comparing snd)
