db224987e58a9193566ff34bdcb40f4770c3dd36
Douglas McClean <douglas.mcclean@gmail.com>
2015-12-04 16:06:06 -0500

    Added sketch of presentation formats.

--- src/Numeric/Units/Dimensional/Presentation.hs
+++ src/Numeric/Units/Dimensional/Presentation.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Numeric.Units.Dimensional.Presentation
-
+(
+  -- * Presentation Quantities
+  PresentationQuantity(..)
+  -- * Presentation Formats
+, PresentationFormat(..)
+, defaultFormat
+)
 where
 
 import Data.Data
@@ -14,4 +21,8 @@ data PresentationQuantity d a = Simple a (Unit 'NonMetric d a)
                               | Composite Integer (Unit 'NonMetric d a) (PresentationQuantity d a)
   deriving (Eq, Generic, Generic1, Typeable)
 
+data PresentationFormat d a = SimpleFormat (a -> String) (Unit 'NonMetric d a)
+                            | CompositeFormat (Unit 'NonMetric d a) (PresentationFormat d a)
 
+defaultFormat :: (Num a, Show a, KnownDimension d) => PresentationFormat d a
+defaultFormat = SimpleFormat show siUnit
