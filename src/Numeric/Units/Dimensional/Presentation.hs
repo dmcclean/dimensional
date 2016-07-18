161708557e4e5d4be629e8e53cb5915c7a278d16
Douglas McClean <douglas.mcclean@gmail.com>
2016-07-18 16:48:04 -0400

    Progress on choosing appropriate prefixes.

--- src/Numeric/Units/Dimensional/Presentation.hs
+++ src/Numeric/Units/Dimensional/Presentation.hs
@@ -22,17 +22,30 @@ import Data.Data
 import GHC.Generics
 import Numeric (showFFloat, showEFloat)
 import Numeric.Units.Dimensional
+import Numeric.Units.Dimensional.SIUnits
 
 data PresentationQuantity d a = Simple a (Unit 'NonMetric d a)
                               | Composite Integer (Unit 'NonMetric d a) (PresentationQuantity d a)
   deriving (Eq, Generic, Generic1, Typeable)
 
-data PresentationFormat d a = SimpleFormat (a -> String -> String) (Unit 'NonMetric d a)
+data PresentationFormat d a = SimpleFormat (a -> String -> String) (PresentationUnit d a)
                             | CompositeFormat (Unit 'NonMetric d a) (PresentationFormat d a)
+
+data PresentationUnit d a = SimpleUnit (Unit 'NonMetric d a)
+                          | PrefixedUnit (Unit 'Metric d a)
+                          | PrefixedUnitMajor (Unit 'Metric d a)
+
+simpleUnit :: Unit m d a -> PresentationUnit d a
+simpleUnit = SimpleUnit . weaken
+
+chooseUnit :: (RealFrac a, Floating a) => PresentationUnit d a -> Quantity d a -> Unit 'NonMetric d a
+chooseUnit (SimpleUnit u)        _ = u
+chooseUnit (PrefixedUnit u)      q = withAppropriatePrefix u q
+chooseUnit (PrefixedUnitMajor u) q = withAppropriatePrefix' u q
 
 -- | Constructs a 'PresentationFormat' from a showing function and a 'Unit'.
 simpleFormat :: (a -> ShowS) -> Unit m d a -> PresentationFormat d a
-simpleFormat s u = SimpleFormat s (weaken u)
+simpleFormat s u = SimpleFormat s (simpleUnit u)
 
 -- | Creates a 'PresentationFormat' for a 'Show'able representation from a specified 'Unit'.
 defaultFormatIn :: (Num a, Show a) => Unit m d a -> PresentationFormat d a
