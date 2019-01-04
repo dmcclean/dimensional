345b3970d539519bd7b352025614ba97bb34dfa7
Douglas McClean <douglas.mcclean@gmail.com>
2019-01-04 07:05:50 -0500

    Removed phantom type index from UnitAtom. Introduced functions for converting UnitNames to normal forms.

--- src/Numeric/Units/Dimensional/UnitNames/Atoms.hs
+++ src/Numeric/Units/Dimensional/UnitNames/Atoms.hs
@@ -6,12 +6,12 @@
 
 module Numeric.Units.Dimensional.UnitNames.Atoms
 (
-  NameAtomType(..)
-, NameAtom
+  NameAtom
 , PrefixName
 , atom
 , nameComponent
 , definiteNameComponent
+, NameMolecule(..)
 )
 where
 
@@ -22,25 +22,24 @@ import Data.Data (Data, Typeable)
 import qualified Data.Map.Strict as M
 import GHC.Generics (Generic)
 import Numeric.Units.Dimensional.UnitNames.Languages
-import Numeric.Units.Dimensional.Variants (Metricality(..))
-
-data NameAtomType = UnitAtom Metricality
-                  | PrefixAtom
-  deriving (Eq, Ord, Data, Typeable, Generic, NFData)
 
 -- | The name of a metric prefix.
-type PrefixName = NameAtom 'PrefixAtom
+type PrefixName = NameAtom
 
 -- | Represents the name of an atomic unit or prefix.
-newtype NameAtom (m :: NameAtomType) 
+newtype NameAtom
   = NameAtom (M.Map Language String) -- It's an invariant that internationalEnglish and internationalEnglishAbbreviation must appear as keys in the map. If the 'NameAtomType' is a prefix or metric, it must also contain 'ucum' as a key.
-  deriving (Eq, Data, Typeable, Generic, NFData)
+  deriving (Eq, Ord, Data, Typeable, Generic, NFData)
+
+data NameMolecule
+  = NameMolecule (Maybe PrefixName) NameAtom
+  deriving (Eq, Ord, Data, Typeable, Generic, NFData)
 
 -- | Constructs a 'NameAtom' of some 'NameAtomType'.
 atom :: String -- ^ Abbreviated name in international English
      -> String -- ^ Full name in international English
      -> [(Language, String)] -- ^ List of names in other 'Language's.
-     -> NameAtom t
+     -> NameAtom
 atom a f ns = NameAtom $ M.union (M.fromList ns) ascii'
   where
     ascii' = if (isValidAscii f) then M.insert internationalEnglishAscii f ascii else ascii
@@ -49,8 +48,8 @@ atom a f ns = NameAtom $ M.union (M.fromList ns) ascii'
     isValidAscii :: String -> Bool
     isValidAscii = all (\c -> C.isAscii c && C.isPrint c)
 
-definiteNameComponent :: Language -> NameAtom m -> String
+definiteNameComponent :: Language -> NameAtom -> String
 definiteNameComponent l (NameAtom m) = m M.! l
 
-nameComponent :: Language -> NameAtom m -> Maybe String
+nameComponent :: Language -> NameAtom -> Maybe String
 nameComponent l (NameAtom m) = M.lookup l m
--- src/Numeric/Units/Dimensional/UnitNames/Internal.hs
+++ src/Numeric/Units/Dimensional/UnitNames/Internal.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE AutoDeriveTypeable #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -19,9 +20,10 @@ where
 import Control.Applicative
 import Control.DeepSeq
 import Control.Monad (join)
-import Data.Coerce (coerce)
 import Data.Data hiding (Prefix)
 import Data.Foldable (toList)
+import qualified Data.Map as M
+import Data.Maybe (isJust)
 import Data.Ord
 import GHC.Generics hiding (Prefix)
 import Numeric.Units.Dimensional.Dimensions.TermLevel (Dimension', asList, HasDimension(..))
@@ -31,44 +33,36 @@ import Numeric.Units.Dimensional.Variants (Metricality(..))
 import Prelude hiding ((*), (/), (^), product)
 import qualified Prelude as P
 
-type UnitName = UnitName' NameAtom
+-- | The name of a unit with the default choice of 'NameAtom' representation.
+type UnitName m = UnitName' m NameAtom
 
--- | The name of a unit.
-data UnitName' (a :: NameAtomType -> *) (m :: Metricality) where
+-- | The name of a unit, parameterized by a type constructor for name atoms of a certain 'NameAtomType' and by the
+-- 'Metricality' of the resulting unit name.
+data UnitName' (m :: Metricality) (a :: *) where
   -- The name of the unit of dimensionless values.
-  One :: UnitName' a 'NonMetric
+  One :: UnitName' 'NonMetric a
   -- A name of an atomic unit to which metric prefixes may be applied.
-  MetricAtomic :: a ('UnitAtom 'Metric) -> UnitName' a 'Metric
+  MetricAtomic :: a -> UnitName' 'Metric a
   -- A name of an atomic unit to which metric prefixes may not be applied.
-  Atomic :: a ('UnitAtom 'NonMetric) -> UnitName' a 'NonMetric
+  Atomic :: a -> UnitName' 'NonMetric a
   -- A name of a prefixed unit.
-  Prefixed :: a 'PrefixAtom -> UnitName' a 'Metric -> UnitName' a 'NonMetric
+  Prefixed :: a -> a -> UnitName' 'NonMetric a
   -- A compound name formed from the product of two names.
-  Product :: UnitName' a 'NonMetric -> UnitName' a 'NonMetric -> UnitName' a 'NonMetric
+  Product :: UnitName' 'NonMetric a -> UnitName' 'NonMetric a -> UnitName' 'NonMetric a
   -- A compound name formed from the quotient of two names.
-  Quotient :: UnitName' a 'NonMetric -> UnitName' a 'NonMetric -> UnitName' a 'NonMetric
+  Quotient :: UnitName' 'NonMetric a -> UnitName' 'NonMetric a -> UnitName' 'NonMetric a
   -- A compound name formed by raising a unit name to an integer power.
-  Power :: UnitName' a 'NonMetric -> Int -> UnitName' a 'NonMetric
+  Power :: UnitName' 'NonMetric a -> Int -> UnitName' 'NonMetric a
   -- A compound name formed by grouping another name, which is generally compound.
-  Grouped :: UnitName' a 'NonMetric -> UnitName' a 'NonMetric
+  Grouped :: UnitName' 'NonMetric a -> UnitName' 'NonMetric a
   -- A weakened name formed by forgetting that it could accept a metric prefix.
   --
   -- Also available is the smart constructor `weaken` which accepts any `UnitName` as input.
-  Weaken :: UnitName' a 'Metric -> UnitName' a 'NonMetric
+  Weaken :: UnitName' 'Metric a -> UnitName' 'NonMetric a
   deriving (Typeable)
 
-deriving instance Eq (UnitName m)
-
-nmap :: (forall t. a t -> b t) -> UnitName' a m -> UnitName' b m
-nmap _ One = One
-nmap f (MetricAtomic a) = MetricAtomic (f a)
-nmap f (Atomic a) = Atomic (f a)
-nmap f (Prefixed p u) = Prefixed (f p) (nmap f u)
-nmap f (Product u1 u2) = Product (nmap f u1) (nmap f u2)
-nmap f (Quotient u1 u2) = Quotient (nmap f u1) (nmap f u2)
-nmap f (Power u n) = Power (nmap f u) n
-nmap f (Grouped u) = Grouped (nmap f u)
-nmap f (Weaken u) = Weaken (nmap f u)
+deriving instance Functor (UnitName' m)
+deriving instance (Eq a) => Eq (UnitName' m a)
 
 -- As it is for a GADT, this instance cannot be derived or use the generic default implementation
 instance NFData (UnitName m) where
@@ -87,17 +81,17 @@ instance NFData (UnitName m) where
 instance Show (UnitName m) where
   show = abbreviation_en
 
-stringName :: (forall t.NameAtom t -> String) -> (forall a.HasUnitName a => a -> String)
-stringName f = go . unitName
+stringName :: (NameAtom -> String) -> (forall a.HasUnitName a => a -> String)
+stringName f = go . fmap f . unitName
   where
-    go :: UnitName m -> String
+    go :: UnitName' m String -> String
     go One = "1"
-    go (MetricAtomic a) = f a
-    go (Atomic a) = f a
-    go (Prefixed a n) = f a ++ show n
+    go (MetricAtomic a) = a
+    go (Atomic a) = a
+    go (Prefixed a n) = a ++ n
     go (Product n1 n2) = go n1 ++ "\xA0" ++ go n2
-    go (Quotient n1 n2) = go n1 ++ "\xA0/\xA0" ++ go n2 -- TODO: handle the case where grouping is required
-    go (Power x n) = go x ++ "^" ++ show n -- TODO: handle the case where grouping is required
+    go (Quotient n1 n2) = go n1 ++ "\xA0/\xA0" ++ go (asSimple n2)
+    go (Power x n) = go (asSimple x) ++ "^" ++ show n
     go (Grouped n) = "(" ++ go n ++ ")"
     go (Weaken n) = go n
 
@@ -106,12 +100,6 @@ class HasUnitName a where
 
 instance HasUnitName (UnitName m) where
   unitName = weaken
-
-instance HasUnitName (NameAtom ('UnitAtom 'Metric)) where
-  unitName = Weaken . MetricAtomic
-
-instance HasUnitName (NameAtom ('UnitAtom 'NonMetric)) where
-  unitName = Atomic
 
 abbreviation_en :: (HasUnitName a) => a -> String
 abbreviation_en = stringName $ definiteNameComponent internationalEnglishAbbreviation
@@ -125,17 +113,129 @@ prefixAbbreviationEnglish = definiteNameComponent internationalEnglishAbbreviati
 prefixNameEnglish :: Prefix -> String
 prefixNameEnglish = definiteNameComponent internationalEnglish . prefixName
 
-asAtomic :: UnitName m -> Maybe (NameAtom ('UnitAtom m))
+asAtomic :: UnitName m -> Maybe NameAtom
 asAtomic (MetricAtomic a) = Just a
 asAtomic (Atomic a) = Just a
-asAtomic (Weaken n) = coerce <$> asAtomic n
+asAtomic (Weaken n) = asAtomic n
 asAtomic _ = Nothing
 
+-- | A 'UnitName' is atomic if it is an atom or a weakening of an atom.
 isAtomic :: UnitName m -> Bool
-isAtomic One = True
-isAtomic (MetricAtomic _) = True
-isAtomic (Atomic _) = True
-isAtomic _ = False
+isAtomic = isJust . asAtomic
+
+asMolecular :: UnitName m -> Maybe NameMolecule
+asMolecular (MetricAtomic a) = Just (NameMolecule Nothing a)
+asMolecular (Atomic a) = Just (NameMolecule Nothing a)
+asMolecular (Prefixed p a) = Just (NameMolecule (Just p) a)
+asMolecular (Weaken n) = asMolecular n
+asMolecular _ = Nothing
+
+isMolecular :: UnitName m -> Bool
+isMolecular = isJust . asMolecular
+
+-- | A 'UnitName' is simple if it is 'One', an atomic name, a prefix application to an atomic name,
+-- a grouping, or a weakening of a simple name.
+isSimple :: UnitName' m a -> Bool
+isSimple One = True
+isSimple (MetricAtomic _) = True
+isSimple (Atomic _) = True
+isSimple (Prefixed _ _) = True
+isSimple (Grouped _) = True
+isSimple (Weaken n) = isSimple n
+isSimple _ = False
+
+-- | Convert a 'UnitName' to one which is simple by grouping it if necessary.
+asSimple :: UnitName' m a -> UnitName' 'NonMetric a
+asSimple n | isSimple n = weaken n
+           | otherwise = grouped n
+
+-- | Convert a 'UnitName' to one in which explicit grouping expressions do not appear.
+eliminateGrouping :: UnitName' m a -> UnitName' m a
+eliminateGrouping (Product n1 n2) = Product (eliminateGrouping n1) (eliminateGrouping n2)
+eliminateGrouping (Quotient n1 n2) = Quotient (eliminateGrouping n1) (eliminateGrouping n2)
+eliminateGrouping (Power n x) = Power (eliminateGrouping n) x
+eliminateGrouping (Grouped n) = n
+eliminateGrouping (Weaken n) = Weaken (eliminateGrouping n)
+eliminateGrouping n = n
+
+-- | Convert a 'UnitName' to one in which inessential uses of 'One' have been eliminated.
+--
+-- Where 'One' appears in a 'Power' or 'Grouped' expression, that expression is eliminated.
+--
+-- A usage is essential if it is alone, or if it is alone in the numerator of a quotient.
+eliminateOnes :: UnitName' m a -> UnitName' m a
+eliminateOnes (Product n1 n2) = case (eliminateOnes n1, eliminateOnes n2) of
+                                  (One, One) -> One
+                                  (One, n) -> n
+                                  (n, One) -> n
+                                  (n1', n2') -> Product n1' n2'
+eliminateOnes (Quotient n1 n2) = case eliminateOnes n2 of
+                                   One -> eliminateOnes n1
+                                   n2' -> Quotient (eliminateOnes n1) n2'
+eliminateOnes (Power n x) = case eliminateOnes n of
+                              One -> One
+                              n' -> Power n' x
+eliminateOnes (Grouped n) = case eliminateOnes n of
+                              One -> One
+                              n' -> Grouped n'
+eliminateOnes (Weaken n) = Weaken (eliminateOnes n)
+eliminateOnes n = n
+
+productNormalForm :: UnitName m -> UnitName m
+productNormalForm n = case n of
+                        (MetricAtomic _) -> n
+                        One -> n
+                        Atomic _ -> n
+                        Prefixed _ _ -> n
+                        Product _ _ -> go n
+                        Quotient _ _ -> go n
+                        Power _ _ -> go n
+                        Grouped _ -> go n
+                        Weaken _ -> go n
+  where
+    go :: UnitName 'NonMetric -> UnitName 'NonMetric
+    go = productOfMolecules . toMolecules
+
+quotientNormalForm :: UnitName m -> UnitName m
+quotientNormalForm n = case n of
+                         (MetricAtomic _) -> n
+                         One -> n
+                         Atomic _ -> n
+                         Prefixed _ _ -> n
+                         Product _ _ -> go n
+                         Quotient _ _ -> go n
+                         Power _ _ -> go n
+                         Grouped _ -> go n
+                         Weaken _ -> go n
+  where
+    go :: UnitName 'NonMetric -> UnitName 'NonMetric
+    go = fromMolecules . partitionMolecules . toMolecules
+    partitionMolecules :: M.Map NameMolecule Int -> (M.Map NameMolecule Int, M.Map NameMolecule Int)
+    partitionMolecules = M.partition (> 0) . M.filter (/= 0)
+    fromMolecules :: (M.Map NameMolecule Int, M.Map NameMolecule Int) -> UnitName 'NonMetric
+    fromMolecules (ns, ds) | M.null ds = productOfMolecules ns
+                           | otherwise = Quotient (productOfMolecules ns) (productOfMolecules $ fmap negate ds)
+
+productOfMolecules :: M.Map NameMolecule Int -> UnitName 'NonMetric
+productOfMolecules = product . fmap build . M.toList
+  where
+    build :: (NameMolecule, Int) -> UnitName 'NonMetric
+    build (_, 0) = One
+    build ((NameMolecule (Just p) a), 1) = Prefixed p a
+    build ((NameMolecule Nothing a), 1) = Atomic a
+    build ((NameMolecule (Just p) a), x) = Power (Prefixed p a) x
+    build ((NameMolecule Nothing a), x) = Power (Atomic a) x
+
+toMolecules :: UnitName m -> M.Map NameMolecule Int
+toMolecules One = M.empty
+toMolecules (Atomic n) = M.singleton (NameMolecule Nothing n) 1
+toMolecules (MetricAtomic n) = M.singleton (NameMolecule Nothing n) 1
+toMolecules (Prefixed p n) = M.singleton (NameMolecule (Just p) n) 1
+toMolecules (Product n1 n2) = M.unionWith (+) (toMolecules n1) (toMolecules n2)
+toMolecules (Quotient n1 n2) = M.unionWith (+) (toMolecules n1) (fmap negate $ toMolecules n2)
+toMolecules (Power n x) = fmap (P.* x) $ toMolecules n
+toMolecules (Grouped n) = toMolecules n
+toMolecules (Weaken n) = toMolecules n
 
 -- reduce by algebraic simplifications
 {-# DEPRECATED reduce "This function has strange and undocumented semantics, and will be removed in favor of more clearly documented algebraic maniupulation functions." #-}
@@ -241,7 +341,7 @@ siPrefixes = [yocto, zepto, atto, femto, pico, nano, micro, milli, centi, deci, 
 
 -- | Forms a 'UnitName' from a 'Metric' name by applying a metric prefix.
 applyPrefix :: Prefix -> UnitName 'Metric -> UnitName 'NonMetric
-applyPrefix = Prefixed . prefixName
+applyPrefix p (MetricAtomic n) = Prefixed (prefixName p) n
 
 {-
 We will reuse the operators and function names from the Prelude.
@@ -266,7 +366,7 @@ x ^ n = Power (weaken x) n
 
 -- | Convert a 'UnitName' which may or may not be 'Metric' to one
 -- which is certainly 'NonMetric'.
-weaken :: UnitName m -> UnitName 'NonMetric
+weaken :: UnitName' m a -> UnitName' 'NonMetric a
 weaken n@(MetricAtomic _) = Weaken n -- we really only need this one case and a catchall, but the typechecker can't see it
 weaken n@One = n
 weaken n@(Atomic _) = n
@@ -279,7 +379,7 @@ weaken n@(Weaken _) = n
 
 -- | Attempt to convert a 'UnitName' which may or may not be 'Metric' to one
 -- which is certainly 'Metric'.
-strengthen :: UnitName m -> Maybe (UnitName 'Metric)
+strengthen :: UnitName' m a -> Maybe (UnitName' 'Metric a)
 strengthen n@(MetricAtomic _) = Just n
 strengthen (Weaken n) = strengthen n
 strengthen _ = Nothing
@@ -287,20 +387,20 @@ strengthen _ = Nothing
 -- | Convert a 'UnitName' of one 'Metricality' into a name of another metricality by
 -- strengthening or weakening if neccessary. Because it may not be possible to strengthen,
 -- the result is returned in a 'Maybe' wrapper.
-relax :: forall m1 m2.(Typeable m1, Typeable m2) => UnitName m1 -> Maybe (UnitName m2)
+relax :: forall m1 m2 a.(Typeable m1, Typeable m2, Typeable a) => UnitName' m1 a -> Maybe (UnitName' m2 a)
 relax = go (typeRep (Proxy :: Proxy m1)) (typeRep (Proxy :: Proxy m2))
   where
     metric = typeRep (Proxy :: Proxy 'Metric)
     nonMetric = typeRep (Proxy :: Proxy 'NonMetric)
-    go :: TypeRep -> TypeRep -> UnitName m1 -> Maybe (UnitName m2)
+    go :: TypeRep -> TypeRep -> UnitName' m1 a -> Maybe (UnitName' m2 a)
     go p1 p2 | p1 == p2 = cast
-             | (p1 == nonMetric) && (p2 == metric) = join . fmap gcast . strengthen
+             | (p1 == nonMetric) && (p2 == metric) = join . fmap cast . strengthen
              | (p1 == metric) && (p2 == nonMetric) = cast . weaken
              | otherwise = error "Should be unreachable. TypeRep of an unexpected Metricality encountered."
 
 -- | Constructs a 'UnitName' by applying a grouping operation to
 -- another 'UnitName', which may be useful to express precedence.
-grouped :: UnitName m -> UnitName 'NonMetric
+grouped :: UnitName' m a -> UnitName' 'NonMetric a
 grouped = Grouped . weaken
 
 ucumName :: (HasUnitName a) => a -> Maybe String
@@ -310,7 +410,7 @@ ucumName = ucumName' . unitName
     ucumName' One = Just "1"
     ucumName' (MetricAtomic a) = nameComponent ucum a
     ucumName' (Atomic a) = nameComponent ucum a
-    ucumName' (Prefixed p n) = (++) <$> nameComponent ucum p <*> ucumName' n
+    ucumName' (Prefixed p n) = (++) <$> nameComponent ucum p <*> nameComponent ucum n
     ucumName' (Product n1 n2) = do
                                   n1' <- ucumName' n1
                                   n2' <- ucumName' n2
